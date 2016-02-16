c2dc3168060e8945638b3e77d5153b19ebfe201c
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2016-02-15 22:44:07 -0500

    Move files
    Jacob adding Chess::Move objects requires I properly namespace my files

---
+++ games/chess/BixNix/MTDFTTNode.h
@@ -0,0 +1,33 @@
+#ifndef _MTDF_TTNODE_H_
+#define _MTDF_TTNODE_H_
+
+#include <climits>
+
+#include "Enums.h"
+#include "Move.h"
+
+namespace BixNix
+{
+    
+class MTDFTTNode
+{
+public:
+    MTDFTTNode() :
+        _hash(0xFFFFFFFFFFFFFFFFLL),
+        _lower(INT_MIN),
+        _upper(INT_MAX),
+        _depth(0),
+        _move(Move()) {}
+
+    ZobristNumber _hash;
+    int _lower;
+    int _upper;
+    unsigned int _depth;
+    Move _move;    
+};
+
+
+}
+
+
+#endif // _TRANSPOSITION_NODE_H_
