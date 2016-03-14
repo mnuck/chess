857fa06013d0cd1020891fe1b4802eb6a9ec5133
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2016-03-14 11:36:51 -0400

    Fix ThreefoldTable to work as advertised
    Unordered_multiset .insert(key) and .erase(key) are not inverses. To
    remove a single key, one must grab an iterator to the key and
    erase that. .erase(key) removes all instances of that key, not
    just one.

--- games/chess/BixNix/ThreefoldTable.cpp
+++ games/chess/BixNix/ThreefoldTable.cpp
@@ -4,7 +4,10 @@ namespace BixNix {
 
 void ThreefoldTable::add(const ZobristNumber key) { _table.insert(key); }
 
-void ThreefoldTable::remove(const ZobristNumber key) { _table.erase(key); }
+void ThreefoldTable::remove(const ZobristNumber key) {
+  auto it = _table.find(key);
+  if (_table.end() != it) _table.erase(it);
+}
 
 bool ThreefoldTable::addWouldTrigger(const ZobristNumber key) const {
   return (_table.count(key) > 1);
